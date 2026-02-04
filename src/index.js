6a8db933ea934a6458106dcbb54327aa4c0f2b64
Himanshu Yadav <iamhimanshu1926@gmail.com>
2026-02-04 15:32:22 +0530

    mongo db connected

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 import dotenv from "dotenv"
+// import express from "express"
+import connectDB from "./db/index.js"
+
+import app from "./app.js"
 
 dotenv.config({
 path:"./.env",
 });
-let myusername=process.env.upyokta_naam
-let mydatabase=process.env.jankari
 
-console.log("value: ",myusername);
-console.log("value: ",mydatabase);
+// const app = express()
+const port = process.env.PORT || 3000
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error", err);
+    process.exit(1);
+  });
 
-console.log("hii hmnsh , welcome to backend");
+
+// app.get('/', (req, res) => {
+//   res.send('Hello hmnsh , this is backend !')
+// })
+
+// app.get("/instagram", (req, res) => {
+//   res.send("this is an instagram page");
+// });
+
+
+
+
+// let myusername=process.env.upyokta_naam
+// let mydatabase=process.env.jankari
+
+// console.log("value: ",myusername);
+// console.log("value: ",mydatabase);
+
+// console.log("hii hmnsh , welcome to backend");
+
+
 
 // What it does: It automatically formats your code.
 //  If you forget a semicolon, use messy indentation, 
